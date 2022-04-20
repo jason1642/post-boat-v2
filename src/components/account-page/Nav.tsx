73649769fb1525323b4c5f68edd29c0f77552a7a
Jason Cruz <jason1642@gmail.com>
2022-04-20 05:30:10 -0400

    style"

--- src/components/account-page/Nav.tsx
+++ src/components/account-page/Nav.tsx
@@ -2,21 +2,37 @@ import _ from 'lodash';
 import * as React from 'react';
 // import { useState, useEffect } from 'react';
 import {Container, Item} from '../../styles/account-page/nav.js'
-
+import {useMatch  } from 'react-router-dom'
 interface INavProps {
   paramsId: string,
 }
-const itemsArray = ['posts', 'comments']
+const optionsData = {
+  posts: {
+    name: 'posts',
+    path:'/user/:id/posts'
+  },
+  comments: {
+    name: 'comments',
+    path: '/user/:id/comments'
+  }
+}
+
 const Nav: React.FunctionComponent<INavProps> = ({paramsId}) => {
 
+  const match = useMatch('/user/:id/comments')
+  // console.log(match)
   return (
     <Container>
 
-      {itemsArray.map((ele, i) =>
+      
         <Item
-          key={i}
-          style={{borderBottom:'1px solid white'}}
-          to={`/user/${paramsId}/${ele}`}>{_.capitalize(ele)}</Item> )}
+          style={{borderBottom: !match && '1px solid white'}}
+        to={`/user/${paramsId}/${optionsData.posts.name}`}>
+        {_.capitalize(optionsData.posts.name)}</Item> 
+        <Item
+          style={{borderBottom: match && '1px solid white'}}
+        to={`/user/${paramsId}/${optionsData.comments.name}`}>
+        {_.capitalize(optionsData.comments.name)}</Item> 
 
 
 
