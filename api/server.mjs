e25ce9b2c1d1eb4ed743096e8caf7ca5d1bfd7c0
Jason Cruz <jason1642@gmail.com>
2022-04-22 05:35:25 -0400

    port

--- api/server.mjs
+++ api/server.mjs
@@ -1,7 +1,7 @@
 import app from './app.mjs'
 import 'dotenv/config';
 import db from './database.mjs';
-const port = process.env.PORT || 3820; 
+const port =  3820; 
 db.connect()
 app.listen(port, () => console.log('listening on port ' + port));
  
