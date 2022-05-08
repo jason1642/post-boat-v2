6aa6ba2738e90ebbdfca2e2596a2656c98b079d1
Jason Cruz <jason1642@gmail.com>
2022-05-08 04:23:02 -0400

    build

--- api/app.mjs
+++ api/app.mjs
@@ -17,26 +17,33 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../build"));
-}
 
-
-app.get('/*', (req, res) => {
-  console.log(path)
-  console.log('path!!!!')
-  console.log(path.join(__dirname, '../build/index.html'))
-  res.sendFile(path.join(__dirname, '../build/index.html'), (err) => {
-    if (err) {
-      res.status(500).send(__dirname)
-    }
-  })
-})
 
 
 
 app.use(express.json());
 app.use(cors());
+
+
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../build"));
+}
+
+
+// app.get('*', (req, res) => {
+//   console.log(path)
+//   console.log('path!!!!')
+//   console.log(path.join(__dirname, '../build/index.html'))
+//   res.sendFile(path.join(__dirname, '../build'), (err) => {
+//     if (err) {
+//       res.status(500).send(__dirname)
+//     }
+//   })
+// })
+
 app.use('/api/auth', authRouter)
 app.use('/api/category', categoryRouter)
 app.use('/api/comment', commentRouter)
